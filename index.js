8630b19ed714360d869f3a6182a33dce78169c61
chinsisme <chinsisme@gmail.com>
2018-07-08 12:56:15 +0530

    Initial commit in database branch

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require('express');
 const app = express();
 const mongo = require('./routes/mongo');
 
+const port = process.env.PORT || 3000 ;
+
 app.use('/api/database', mongo);
+
+app.listen(port, () => console.log('Listening on port ' + port ));
+
+
+
--- routes/mongo.js
+++ routes/mongo.js
@@ -1,9 +1,58 @@
 const express = require('express'); 
 const router = express.Router();
-
-router.get('/', (req, res) => {
-    res.send(console.log('Response received!'));
+const mongoose = require('mongoose');
+const fs = require('fs');
+const jsonFile = fs.readFile('./static/enron.json', (err, data) => {
+    if (err) throw err;
+    return data;
 })
 
+router.use(express.json());
+
+mongoose.connect('mongodb://localhost/nodejs')
+    .then(() => console.log('Connected to MongoDB!'))
+    .catch((err) => console.log('Error',err));
+
+async function importFile (inputFile) {
+    return await enron.save(inputFile);
+}
+
+
+const enronSchema = new mongoose.Schema({
+    sender: String,
+    recipients: String,
+    cc: String,
+    text: String,
+    mid: String,
+    fpath: String,
+    bcc: String,
+    to: String,
+    replyto: String,
+    ctype: String,
+    fname: String,
+    date: String,
+    folder: String,
+    subject: String
+});
+
+enronSchema.set('collection', 'enron');
+
+const Enron = mongoose.model('enron', enronSchema);
+
+
+router.get('/', (req, res) => {
+    res.send('Request received!');
+    console.log('GET request!');
+})
+
+router.post('/importall', (req,res,callback) => {
+    // const enron = new Enron(jsonFile);
+    // send to MongoDB
+    res.send('Response received');
+    const enron = new Enron(jsonFile);
+    const result = enron.save();
+    console.log(enron);
+    
+})
 
 module.exports = router;
