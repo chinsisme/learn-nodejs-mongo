a4d4324bba7b5f20361f92b1150d86815258c0cf
chinsisme <chinsisme@gmail.com>
2018-07-08 14:44:29 +0530

    Beginning to create DB server

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,11 +1,6 @@
 const express = require('express'); 
 const router = express.Router();
 const mongoose = require('mongoose');
-const fs = require('fs');
-const jsonFile = fs.readFile('./static/enron.json', (err, data) => {
-    if (err) throw err;
-    return data;
-})
 
 router.use(express.json());
 
@@ -13,46 +8,33 @@ mongoose.connect('mongodb://localhost/nodejs')
     .then(() => console.log('Connected to MongoDB!'))
     .catch((err) => console.log('Error',err));
 
-async function importFile (inputFile) {
-    return await enron.save(inputFile);
-}
 
+// const enronSchema = new mongoose.Schema({
+//     sender: String,
+//     recipients: String,
+//     cc: String,
+//     text: String,
+//     mid: String,
+//     fpath: String,
+//     bcc: String,
+//     to: String,
+//     replyto: String,
+//     ctype: String,
+//     fname: String,
+//     date: String,
+//     folder: String,
+//     subject: String
+// });
 
-const enronSchema = new mongoose.Schema({
-    sender: String,
-    recipients: String,
-    cc: String,
-    text: String,
-    mid: String,
-    fpath: String,
-    bcc: String,
-    to: String,
-    replyto: String,
-    ctype: String,
-    fname: String,
-    date: String,
-    folder: String,
-    subject: String
-});
+// enronSchema.set('collection', 'enron');
 
-enronSchema.set('collection', 'enron');
-
-const Enron = mongoose.model('enron', enronSchema);
+// const Enron = mongoose.model('enron', enronSchema);
 
 
 router.get('/', (req, res) => {
-    res.send('Request received!');
-    console.log('GET request!');
+    res.send('Welcome to Chinmay\'s MongoDB server!');
+    console.log('GET Request received.');
 })
 
-router.post('/importall', (req,res,callback) => {
-    // const enron = new Enron(jsonFile);
-    // send to MongoDB
-    res.send('Response received');
-    const enron = new Enron(jsonFile);
-    const result = enron.save();
-    console.log(enron);
-    
-})
 
 module.exports = router;
